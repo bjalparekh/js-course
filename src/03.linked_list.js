77a3d91ebc3853e3bdf27d18fcb580c58f5a73ff
Koushik Shetty <kou4307@gmail.com>
2018-03-25 21:22:53 +0530

    fix linked list issue

--- src/03.linked_list.js
+++ src/03.linked_list.js
@@ -6,12 +6,12 @@ class Node{
 }
 
 module.exports = {
-	linked_list: function(root) {
-		// write code to find the kth element from the end of a linked list
+	linked_list: function(arr = []) {
+		// write code to create a linked list
 		// e.g 
-		// input = 5, 8, 3, 7, 2, 1, 9, 10, 6
+		// input = [5, 8, 3, 7, 2, 1, 9, 10, 6]
 		// k = 4
-		// output = 1
+		// output = root pointing to the initial node
 		throw "NotImplemented"
 	},
 	Node: Node,

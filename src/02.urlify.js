ae7a7e1527a18811d0f898b3ecdd5c00c1a833e5
Bijal <bijalbkp@thoughtworks.com>
2018-03-31 08:20:39 +0530

    Add trim and replace function

--- src/02.urlify.js
+++ src/02.urlify.js
@@ -1,4 +1,42 @@
 module.exports = function urlify(str) {
-	return str.trim().replace(/\s+/g, '%20');
+	return str.trimmer().replaced(' ', '%20');
 	throw "NotImplemented"
 }
+
+
+String.prototype.replaced = function(a, b) {
+	const subStr = new String(a);
+	let str = this
+	for(let i =0;i<str.length;i++) {
+		for(let k = 0; k<subStr.length; k++) {
+			if(str[i] === subStr[k]) {
+				if(str.substr(i, subStr.length) === a) {
+          let newStr = str.substr(0, i);
+					str = newStr.concat(b).concat(str.substr(i+subStr.length));
+				}
+			}
+		}
+	}
+
+  return str
+}
+
+String.prototype.trimmer = function() {
+	let _self = this;
+	for(let i=0;i<_self.length;i++) {
+		if(_self[i] !== " ") {
+			_self = _self.substr(i)
+			break;
+		}
+	}
+  console.log(_self.length)
+  for(let i=_self.length-1;i>-1;i--) {
+    if(_self[i] !== ' ') {
+      return _self.substr(0, i+1)
+    }
+  }
+}
+
+var a = '  Bijal  '.trimmer()
+
+console.log(a)
